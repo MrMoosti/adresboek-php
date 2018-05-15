ad23038f5159653e710a154d442921f698b59f22
BramEsendam <bram.esendam@gmail.com>
2018-05-15 16:49:43 +0200

    Added real contact list

---
+++ siteroot/public/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8" />
+    <title>OnlineAdresboek | Home</title>
+    <link rel="stylesheet" href="stylesheet.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous" />
+</head>
+
+<body>
+    <header>
+        <h2>OnlineAdresboek</h2>
+    </header>
+
+    <!-- ON EVERY PAGE -->
+    <div id="side-panel">
+        <nav>
+            <ul class="menu">
+                <li>
+                    <a href="#">
+                        <div class="sidepanel-button gebruikers-but">
+                            <h4>Gebruikers</h4>
+                            <i class="fas fa-user fa-2x sidepanel-icons"></i>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="sidepanel-button contacten-but">
+                            <h4>Contacten</h4>
+                            <i class="fas fa-users fa-2x sidepanel-icons"></i>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="login.html">
+                        <div class="sidepanel-button uitloggen-but">
+                            <h4>Uitloggen</h4>
+                            <i class="fas fa-key fa-2x sidepanel-icons"></i>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+
+            <a href="#">
+                <div id="drop-down">
+                    <i class="fas fa-angle-down fa-3x"></i>
+                </div>
+            </a>
+
+        </nav>
+    </div>
+    <!-- ON EVERY PAGE -->
+
+
+    <!-- THIS SECTION CHANGES DEPENDEND ON SQL-QUERY -->
+    <section id="content">
+        <div class="search">
+            <form class="searchForm" method="get">
+                <input type="text" name="voornaam" placeholder="Zoek..." />
+                <a href="#">
+                    <i class="fa fa-search"></i>
+                </a>
+            </form>
+        </div>
+        <div class="sortNames">
+            <ul type="none">
+                <li class="voornaam-sorteren">
+                    <a href="#">
+                        voornaam
+                        <i class="fa fa-caret-down"></i>
+                    </a>
+                </li>
+                <li class="achternaam-sorteren">
+                    <a href="#">
+                        achternaam
+                        <i class="fa fa-caret-down"></i>
+                    </a>
+                </li>
+                <li class="plaats-sorteren">
+                    <a href="#">
+                        plaats
+                        <i class="fa fa-caret-down"></i>
+                    </a>
+                </li>
+                <li class="bedrijfsnaam-sorteren">
+                    <a href="#">
+                        bedrijfsnaam
+                        <i class="fa fa-caret-down"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+            <?php include 'listContacts.php';?>
+    </section>
+
+    <section id="detail">
+        <p>Hi</p>
+    </section>
+
+
+    <script src="js/vendor/jquery-3.3.1.min.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
