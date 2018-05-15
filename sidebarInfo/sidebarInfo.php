d8c5b9f9476cbf60397351c9d57d874b2d73cd4f
Biesono <jaccobiezeman@gmail.com>
2018-05-15 11:01:45 +0200

    sidebarInfo
    Kijk ff of die cool is

---
+++ sidebarInfo/sidebarInfo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jacco: Sidebar</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="styleSidebar.css">
+</head>
+<body>
+	<div class="sidebar">
+		<img src="kim.jpg">
+		<h2>Kim, Jung Un</h2>
+		<div class="paraSide">
+			<p><i class="fa fa-address-book" style="color: black"></i> Kim Jung Un</p>
+			<p><i class="fa fa-envelope" style="color: black"></i>kimjungun@gmail.com</p>
+			<p><i class="fa fa-phone" style="color: black"></i>0315-128454823</p>
+			<p><i class="fa fa-user" style="color: black"></i>xXx_KimJungUN_xXx</p>
+			<p><i class="fa fa-key" style="color: black"></i>imnotadictator</p>
+			<p><i class="fa fa-shield" style="color: black"></i>Gebruiker</p>
+		</div>	
+		<div class="deleteChange">
+			<i class="fa fa-pencil"></i>
+			<i class="fa fa-trash"></i>
+		</div>	
+	</div>
+</body>
+</html>
