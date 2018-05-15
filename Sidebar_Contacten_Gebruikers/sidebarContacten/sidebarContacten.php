accebb267506a61034ed1f523bdf55e61fce4136
Biesono <jaccobiezeman@gmail.com>
2018-05-15 11:18:02 +0200

    Alle sidebars
    alle sidebars zitten nu in een file en de sommige filenames zijn veranderd

---
+++ Sidebar_Contacten_Gebruikers/sidebarContacten/sidebarContacten.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jacco: Sidebar</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="styleSidebar.css">
+</head>
+<body>
+	<div class="sidebar">
+		<img src="kim.jpg">
+		<h2>Kim, Jung Un</h2>
+		<div class="paraSide">
+			<p><i class="fa fa-address-book" style="color: black"></i> Kim Jung Un</p>
+			<p><i class="fa fa-building" style="color: black"></i>Microsoft</p>
+			<p><i class="fa fa-envelope" style="color: black"></i>kimjungun@gmail.com</p>
+			<p><i class="fa fa-phone" style="color: black"></i>+31 1238624</p>
+			<p><i class="fa fa-phone" style="color: black"></i>0315-18793712</p>
+			<p><i class="fa fa-building" style="color: black"></i>Ulft</p>
+		</div>	
+		<div class="deleteChange">
+			<i class="fa fa-pencil"></i>
+			<i class="fa fa-trash"></i>
+		</div>	
+	</div>
+</body>
+</html>
