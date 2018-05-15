007e59ae6619211d355748dc8b4291a5bbd20282
MrMoosti <meyilmaz2001@hotmail.com>
2018-05-15 10:27:30 +0200

    sorteren js / responsive with cards implemented

--- siteroot/public/js/main.js
+++ siteroot/public/js/main.js
@@ -1,18 +1,73 @@
 $(document).ready(function() {
 
-  $('.gebruikers-but').click(function(e) {
-
-  });
-
+  //Default open menu
   $('.menu').toggleClass('slide-down');
 
-  $('#drop-down').on('click', function(e) {
-    // Prevent link from jumping to the top of the page
-    e.preventDefault();
-    // If menu is already showing, slide it up. Otherwise, slide it down.
+  /* SORTEREN */
+  var $voornaam = $('.voornaam-sorteren');
+  var $achternaam = $('.achternaam-sorteren');
+  var $plaats = $('.plaats-sorteren');
+  var $bedrijfsnaam = $('.bedrijfsnaam-sorteren');
 
-    $('.menu').toggleClass('slide-down');
+  function defaultSorteer()
+  {
+    if(!$voornaam.hasClass("bold-sorteren")) {
+
+      $voornaam.addClass("bold-sorteren");
+
+      $achternaam.removeClass("bold-sorteren");
+      $plaats.removeClass("bold-sorteren");
+      $bedrijfsnaam.removeClass("bold-sorteren");
+    }
+  };
+
+  defaultSorteer();
+
+  $voornaam.on('click', function(e) {
+    if(!$voornaam.hasClass("bold-sorteren")) {
+
+      $voornaam.addClass("bold-sorteren");
+
+      $achternaam.removeClass("bold-sorteren");
+      $plaats.removeClass("bold-sorteren");
+      $bedrijfsnaam.removeClass("bold-sorteren");
+    }
   });
+
+  $achternaam.on('click', function(e) {
+    if(!$achternaam.hasClass("bold-sorteren")) {
+
+      $achternaam.addClass("bold-sorteren");
+
+      $voornaam.removeClass("bold-sorteren");
+      $plaats.removeClass("bold-sorteren");
+      $bedrijfsnaam.removeClass("bold-sorteren");
+    }
+  });
+
+  $plaats.on('click', function(e) {
+    if(!$plaats.hasClass("bold-sorteren")) {
+
+      $plaats.addClass("bold-sorteren");
+
+      $voornaam.removeClass("bold-sorteren");
+      $achternaam.removeClass("bold-sorteren");
+      $bedrijfsnaam.removeClass("bold-sorteren");
+    }
+  });
+
+  $bedrijfsnaam.on('click', function(e) {
+    if(!$bedrijfsnaam.hasClass("bold-sorteren")) {
+
+      $bedrijfsnaam.addClass("bold-sorteren");
+
+      $voornaam.removeClass("bold-sorteren");
+      $achternaam.removeClass("bold-sorteren");
+      $plaats.removeClass("bold-sorteren");
+    }
+  });
+
+  /* FOR MENU */
 
   // Optimalisation: Store the references outside the event handler:
   var $window = $(window);
@@ -32,4 +87,13 @@ $(document).ready(function() {
   checkWidth();
   // Bind event listener
   $(window).resize(checkWidth);
+
+  $('#drop-down').on('click', function(e) {
+    // Prevent link from jumping to the top of the page
+    e.preventDefault();
+    // If menu is already showing, slide it up. Otherwise, slide it down.
+
+    $('.menu').toggleClass('slide-down');
+  });
+
 });
