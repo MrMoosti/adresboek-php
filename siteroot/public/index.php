c8cefb9920143352b5bf210ee4541fe69c697b45
Bram Esendam <bram.esendam@gmail.com>
2018-05-17 13:03:44 +0200

    OOP IS GWN BETER denk ik

--- siteroot/public/index.php
+++ siteroot/public/index.php
@@ -1,6 +1,6 @@
 <?php include('includes/Contactpersoon.php'); ?>
 <?php include 'layouts/index_header.php'; ?>
-<?php require_once('includes/Database.php'); ?>
+<?php require_once('includes/ContactPersoon.php'); ?>
 <!-- THIS SECTION CHANGES DEPENDEND ON SQL-QUERY -->
 <section id="content">
     <div class="search">
@@ -42,27 +42,16 @@
 
     <div class="listNames">
         <?php
-        global $database;
-        $sql = "SELECT `ID`, `username`, `first_name`, `insertion`, `last_name`, `business_name`, `business_place`, `zipcode`, `email`, `telephone_work`, `telephone_private`, `img_filename`, `img_size`, `img_type` FROM `contactperson` ";
-        $result = $database->query($sql);
-        if ($result->num_rows > 0) {
-            //adding one row per person
-            while ($row = $result->fetch_assoc()) {
-                echo "<div class=\"section\">";
-                echo "<img src=\"{$row["img_filename"]}\" />";
-                echo "<p>{$row["first_name"]}, {$row["last_name"]}</p>";
-                echo "<p>{$row["zipcode"]}, {$row["business_place"]}</p>";
-                echo "<p class=\"sectionCompany\">{$row["business_name"]}</p>";
-                echo "</div><hr />";
-            }
-        } else {
-            echo "  <div class=\"section\">
-  				<img src=\"images/profile_pictures/kim.jpg\">
-  				<p>Kim, Jung Un</p>
-  				<p>7071 KA, Ulft</p>
-  				<p class=\"sectionCompany\">microsoft</p>
-  			    </div><hr></div>";
-        } ?>
+        $result = ContactPersoon::find_all();
+        foreach ($result as $person) {
+            echo "<div class=\"section\">";
+            echo "<img src=\"{$person->img_filename}\" />";
+            echo "<p>{$person->first_name}, {$person->last_name}</p>";
+            echo "<p>{$person->zipcode}, {$person->business_place}</p>";
+            echo "<p class=\"sectionCompany\">{$person->business_name}</p>";
+            echo "</div><hr />";
+        }
+        ?>
     </div>
 
 </section>
